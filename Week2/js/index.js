c5a6d26e28e0ab634be4bc9257df74ae4983210d
woodzverse <woodzverse@naver.com>
2025-10-29 22:59:24 +0900

    chore: 초기 멤버 데이터 추가

---
+++ Week2/js/index.js
@@ -0,0 +1,8 @@
+import { initMembers } from "./storage.js";
+import { renderAllMembers } from "./render.js";
+import "./member.js";
+import "./checkbox.js";
+import "./modal.js";
+
+initMembers();
+renderAllMembers();
